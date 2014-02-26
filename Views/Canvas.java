7939a91e2e6bc26aac0e9b84d7ada93d5f70f4a6
Grzegorz <grzegorz@ubuntu.(none)>
2014-02-26 09:11:25 +0100

    Counting positon of ship

--- Views/Canvas.java
+++ Views/Canvas.java
@@ -1,22 +1,38 @@
 package Views;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Insets;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.util.ArrayList;
 import java.util.Random;
 
+import javax.swing.JComponent;
 import javax.swing.JPanel;
 
 public class Canvas extends JPanel{
+	
+	private int count;
+	private Point coordinates = new Point(0, 0);
+    private ArrayList<Point> coordinateList = new ArrayList<Point>();
+    private int side;
+    
+    public Canvas(int n){
+    	side = n;
+    	setPreferredSize(new Dimension(1000, 1000));
+    }
+	
 	  private void doDrawing(Graphics g) {
 
 	        Graphics2D g2d = (Graphics2D) g;
 
 	        g2d.setColor(Color.blue);
-	        g2d.drawLine(385, 0, 385, 550);
-
+	        g2d.drawLine(381, 0, 381, 585);
 
 	       /* 
 	        * vertical line  (top to bottom centered)
@@ -33,6 +49,49 @@ public class Canvas extends JPanel{
 	        
 	        super.paintComponent(g);
 	        doDrawing(g);
-	    }	
+	        matchRectangle(g, 5, 2);
+	        Graphics2D g2 = (Graphics2D) g;
+
+	        int count = side;
+	        int size = 20;
+	        g2.setColor(Color.black);
+	        for( int i = 0; i < count; i ++)
+	        {
+	          for( int j = 0; j < count-10; j++)
+	          {
+	        	 //Rectangle grid = new Rectangle( 300 + i * size, 20 + j * size, size, size);
+		         Rectangle grid = new Rectangle(i*size, j*size, size, size);
+	        	 g2.draw(grid);
+	        	             
+	          }
+	        }
+	     
+
+	        
+	       } // paintComponent
 	    
-}
+	    private void matchRectangle(Graphics g, int x, int y){
+	    	int dy = getSize ().height;
+	        int dx = getSize ().width;
+	        //int mid_y = dy/2-y;
+	        //int mid_x = dx/2-x;
+	        
+	        int mid_y=0;// = 20+x;
+	        int mid_x=0;// = 20+y;
+	        
+	        for(int i=0; i<x-1; i++){
+	        	mid_x = (mid_x + 20);
+	        	for(int j=0; j<y-1; j++){
+	        		mid_y = (mid_y + 20);
+	        	}
+	        }
+	        
+	    	g.setColor (Color.red);
+	    	g.fillRect(mid_x, mid_y, 20, 20);
+	    }
+	    
+	   
+	    
+	            
+}	
+	   
--- Views/ContainerGame.java
+++ Views/ContainerGame.java
@@ -2,11 +2,14 @@ package Views;
 
 import java.awt.ComponentOrientation;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class ContainerGame extends JPanel{
 	public ContainerGame(Container pane){
@@ -18,36 +21,39 @@ public class ContainerGame extends JPanel{
 		pane.setLayout(new GridBagLayout());
 		
 
-	    Canvas can = new Canvas();
+	    Canvas can = new Canvas(40);
 	    //c.ipady = 500;      //make this component tall
-		
+	    //can.paintComponent();
 
-	    c.weightx = 1;
-	    c.weighty = 4;
+	    c.weightx = 0.65;
+	    c.weighty = 3.2;
 
 	    c.gridx = 1;
 	    c.gridy = 0;
 
 	    pane.add(can, c);
 	    
-		
-		JButton button = new JButton("Button 1");
+	    //JTextField field = new JTextField("Type something........", 0);
+	    JTextArea field = new JTextArea(5, 20);
+	    field.setEditable(false);
+	    //field.setPreferredSize( new Dimension( 200, 24 ) );
+		//JButton button = new JButton("Button 1");
 		//c.fill = GridBagConstraints.HORIZONTAL;
 	    c.weightx = 0.2;
 	    c.weighty = 0.7;
 
 	    c.gridx = 0;
 	    c.gridy = 0;
-	    pane.add(button, c);
+	    pane.add(field, c);
 	    
 	    
 	    
-	    button = new JButton("Button 2");
+	    JButton button = new JButton("Button 2");
 	    //c.fill = GridBagConstraints.HORIZONTAL;
 	    //c.fill = GridBagConstraints.RELATIVE;
 	    c.gridx = 0;
 	    c.gridy = 2;
-c.gridwidth=3;
+	    c.gridwidth=3;
 	    pane.add(button, c);
 	    
 	}
